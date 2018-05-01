5c60e4a21b8ce56b5b9a5114ee5bf211cca40da0
Ricardo Amores <rickyah@gmail.com>
2018-05-02 00:14:41 +0200

    [WIP] Organized tests

--- src/IniFileParser.Tests/Unit/Model/IniSchemeTests.cs
+++ src/IniFileParser.Tests/Unit/Model/IniSchemeTests.cs
@@ -3,10 +3,11 @@ using IniParser.Model.Configuration;
 
 namespace IniParser.Tests.Unit
 {
-    [TestFixture]
+    [TestFixture, Category("DataModel")]
     public class IniSchemeTests
     {        
-        [Test] public static void check_cloning()
+        [Test] 
+        public void check_deep_clone()
         {
             var scheme1 = new IniScheme();
             scheme1.CommentString = "/";
@@ -14,6 +15,73 @@ namespace IniParser.Tests.Unit
 
             var scheme2 = scheme1.DeepClone();
             Assert.That(scheme2.CommentString, Is.EqualTo("/"));
+        }
+        
+        [Test]
+        public void TestCommentRegex()
+        {
+            var scheme = new IniScheme();
+            scheme.CommentString = ";";
+            
+            string strGoodTest1 = ";comment Test";
+            string strGoodTest2 = "   ; comment Test";
+            string strGoodTest3 = "   ; comment Test           ";
+            string strGoodTest4 = " dfasdfasf ; comment Test ";
+
+            Assert.That(strGoodTest1, Does.Match(scheme.CommentRegex.ToString()));
+            Assert.That(strGoodTest2, Does.Not.Match(scheme.CommentRegex.ToString()));
+            Assert.That(strGoodTest3, Does.Not.Match(scheme.CommentRegex.ToString()));
+            Assert.That(strGoodTest4, Does.Not.Match(scheme.CommentRegex.ToString()));
+        }
+
+        [Test,]
+        public void TestSectionRegex()
+        {
+            var scheme = new IniScheme();
+            
+            string strGoodTest1 = "[section]";
+            string strGoodTest2 = "   [sec-tion]   ";
+            string strGoodTest3 = "[ .section ]";
+            string strGoodTest4 = "[ s_ection ]";
+
+            string strBadTest1 = "  bad [section]";
+            string strBadTest2 = "[section] bad";
+
+            Assert.That(strGoodTest1, Does.Match(scheme.SectionRegex.ToString()));
+            Assert.That(strGoodTest2, Does.Match(scheme.SectionRegex.ToString()));
+            Assert.That(strGoodTest3, Does.Match(scheme.SectionRegex.ToString()));
+            Assert.That(strGoodTest4, Does.Match(scheme.SectionRegex.ToString()));
+
+            Assert.That(strBadTest1, Does.Not.Match(scheme.SectionRegex.ToString()));
+            Assert.That(strBadTest2, Does.Not.Match(scheme.SectionRegex.ToString()));
+        }
+
+        [Test, Description("Test a regular expression for matching a section in an INI file given an specific delimiter")]
+        public void TestNewSectionDelimiter()
+        {
+            var scheme = new IniScheme();
+                    
+            scheme.SectionStartString = "<";
+            scheme.SectionEndString = ">";
+
+            string strGoodTest1 = "<section>";
+            string strGoodTest2 = "   <section>   ";
+            string strGoodTest3 = "< section >";
+
+            string strBadTest1 = "  bad <section>";
+            string strBadTest2 = "<section> bad";
+
+
+            Assert.That(strGoodTest1, Does.Match(scheme.SectionRegex.ToString()));
+            Assert.That(strGoodTest2, Does.Match(scheme.SectionRegex.ToString()));
+            Assert.That(strGoodTest3, Does.Match(scheme.SectionRegex.ToString()));
+
+            Assert.That(strBadTest1, Does.Not.Match(scheme.SectionRegex.ToString()));
+            Assert.That(strBadTest2, Does.Not.Match(scheme.SectionRegex.ToString()));
+
+            //Restore default delimiters
+            scheme.SectionStartString = "[";
+            scheme.SectionEndString = "]";
         }
     }
 }
